205de9273f5aebb6bf75b0dfa8c4955ab525849c
FadeevaDasha <genkovna1999@gmail.com>
2025-07-01 14:27:01 +0300

    Pytest skip, skipif, xfail

---
+++ tests/test_pytest_skip.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.skip(reason="Фича в разработке")
+def test_feature_in_development():
+    ...
+
+
+@pytest.mark.skip(reason="Фича в разработке")
+class TestSuiteSkip:
+    def test_feature_in_development_1(self):
+        ...
+
+    def test_feature_in_development_2(self):
+        ...
+
+    def test_feature_in_development_3(self):
+        ...
+
+    def test_feature_in_development_4(self):
+        ...
