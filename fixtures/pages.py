4e0ff26a00e712e2ec0548b7bca6d4dd9d5d15e6
FadeevaDasha <genkovna1999@gmail.com>
2025-07-13 20:39:52 +0300

    Практикуемся в реализации PageObject

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -7,3 +7,11 @@ from pages.login_page import LoginPage
 @pytest.fixture
 def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(page=chromium_page)
+
+@pytest.fixture
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
+
+@pytest.fixture
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
