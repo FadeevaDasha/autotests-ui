18cfe745421559bbce9058577f4bfc7b783de48e
FadeevaDasha <genkovna1999@gmail.com>
2025-07-28 09:46:14 +0300

    Практикуемся в реализации PageObject

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 from pages.base_page import BasePage
 
@@ -7,29 +7,23 @@ class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        # Заголовок и кнопка создания курса
         self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
         self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
 
-        # Карточка курса
         self.course_title = page.get_by_test_id('course-widget-title-text')
         self.course_image = page.get_by_test_id('course-preview-image')
-        self.course_max_text = page.get_by_test_id('course-max-score-info-row-view-text')
-        self.course_min_text = page.get_by_test_id('course-min-score-info-row-view-text')
+        self.course_max_score_text = page.get_by_test_id('course-max-score-info-row-view-text')
+        self.course_min_score_text = page.get_by_test_id('course-min-score-info-row-view-text')
         self.course_estimated_time_text = page.get_by_test_id('course-estimated-time-info-row-view-text')
 
-        # Меню курса
         self.course_menu_button = page.get_by_test_id('course-view-menu-button')
         self.course_edit_menu_item = page.get_by_test_id('course-view-edit-menu-item')
         self.course_delete_menu_item = page.get_by_test_id('course-view-delete-menu-item')
 
-        # Пустой блок при отсутствии курсов
         self.empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
         self.empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
         self.empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
 
-
-    # Методы для страницы без курсов
     def check_visible_courses_title(self):
         expect(self.courses_title).to_be_visible()
         expect(self.courses_title).to_have_text('Courses')
@@ -51,19 +45,16 @@ class CoursesListPage(BasePage):
     def click_create_course_button(self):
         self.create_course_button.click()
 
-
-    # Методы для проверки Карточки
     def check_visible_course_card(
             self,
-            index: int,  # Индекс карточки в списке курсов
-            title: str,  # Ожидаемый заголовок курса
-            max_score: str,  # Ожидаемый максимальный балл
-            min_score: str,  # Ожидаемый минимальный балл
-            estimated_time: str  # Ожидаемое время прохождения
+            index: int,
+            title: str,
+            max_score: str,
+            min_score: str,
+            estimated_time: str
     ):
         expect(self.course_image.nth(index)).to_be_visible()
 
-        # Обратите внимание на использование метода nth, он позволяет получить локатор по индексу
         expect(self.course_title.nth(index)).to_be_visible()
         expect(self.course_title.nth(index)).to_have_text(title)
 
@@ -81,7 +72,6 @@ class CoursesListPage(BasePage):
     def click_edit_course(self, index: int):
         self.course_menu_button.nth(index).click()
 
-        # После нажатия на кнопку меню обязательно проверяем, что меню открылась и меню айтем виден
         expect(self.course_edit_menu_item.nth(index)).to_be_visible()
         self.course_edit_menu_item.nth(index).click()
 
